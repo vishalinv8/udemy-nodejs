2f3b44fcbbee9da3d956c819802f450852faa0d0
Vishal <vishal.khanjan@gmail.com>
2020-03-31 21:40:59 +0530

    Update code

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const routes = require('./routes');
 
-const server = http.createServer((req, res)=>{
-    //console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    if(url === '/'){
-        res.write(`<html>
-        <head> <title> Enter message</title> </head>
-        <body>
-            <form action="/message" method="POST">
-                <input type="text" name="message">
-                <input type="submit">
-            </form>
-        </body>
-        </html>`);
-        return res.end();
-    }
-    //console.log("url", url, "req.method", req.method, req);
-    if(url === '/message' && req.method === 'POST'){
-        const body = [];
-        req.on('data', (chunkData)=>{
-            console.log("chunkData", chunkData);
-            body.push(chunkData);
-        });
-        req.on('end', ()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            console.log("parsedBody", parsedBody);
-            var message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err)=>{
-                res.statusCode = 302;
-                res.setHeader("Location","/");
-                return res.end();
-            });            
-        });
-    }
-    res.setHeader("Content-type", "text/html");
-    res.write(`<html>
-    <head> <title> First node Application</title> </head>
-    <body><h1> Hello NODE js server</h1></body>
-    </html>`);
-    res.end();
-});
+const server = http.createServer(routes.handler);
 server.listen(2020);
---
+++ routes.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write(`<html>
+        <head> <title> Enter message</title> </head>
+        <body>
+            <form action="/message" method="POST">
+                <input type="text" name="message">
+                <input type="submit">
+            </form>
+        </body>
+        </html>`);
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunkData)=>{
+            body.push(chunkData);
+        });
+        req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            var message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err)=>{
+                if(!err){
+                    
+                }
+                else{
+                    console.log("writeFile error", err);
+                }
+            });
+        });
+        res.statusCode = 302;
+        res.setHeader("Location","/");
+        return res.end();
+    }
+    res.setHeader("Content-type", "text/html");
+    res.write(`<html>
+    <head> <title> First node Application</title> </head>
+    <body><h1> Hello NODE js server</h1></body>
+    </html>`);
+    res.end();
+};
+
+module.exports = {
+    handler: requestHandler,
+    someText:'Some hardcode text here'
+};
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text here';
+
+// exports.handler = requestHandler;
+// exports.someText = 'Some hardcoded string';
+
+
+
