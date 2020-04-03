4a268be9874b2ca4c0306b65067b424265877ee4
Vishal <vishal.khanjan@gmail.com>
2020-04-03 23:29:13 +0530

    Practice updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
+const app = express(); // import express module as function
 const routes = require('./routes');
-
 const server = http.createServer(routes.handler);
 server.listen(2020);
