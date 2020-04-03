0a24343c1ac6df57f0efdbcd22dd2f64184428ed
Vishal <vishal.khanjan@gmail.com>
2020-04-04 00:01:17 +0530

    Practice updated

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
 const express = require("express");
-const app = express(); // import express module as function
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
-server.listen(2020);
+const app = express(); 
+
+
+app.use('/', (req, res, next)=>{
+    console.log("This Always Runs!");
+    next();
+});
+
+
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`<h1>Add product form</h1>`);
+});
+
+app.use('/', (req, res, next) => {
+    res.send(`<h1> Home page </h1>`);
+});
+
+app.listen(2020);
