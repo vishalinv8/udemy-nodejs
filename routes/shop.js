36422cd6c73f785375a07cf790f62f7bd58c8bc9
Vishal <vishal.khanjan@gmail.com>
2020-04-10 21:43:01 +0530

    Practice updated

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,8 @@ const router = express.Router();
 router.get('/', (req, res, next)=>{
     //console.log("shop.js", adminData.products);
     //res.sendFile(path.join(rootDir, "views", "shop.html"));
-    res.render("shop");
+    const products = adminData.products;
+    res.render("shop", {prods: products, docTitle:"Ecommerce"});
 });
 
 module.exports = router;
