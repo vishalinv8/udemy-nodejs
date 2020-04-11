d15e044fe975ac28e2a3f9919522850433b72b72
Vishal <vishal.khanjan@gmail.com>
2020-04-11 13:20:38 +0530

    working with controllers

--- app.js
+++ app.js
@@ -10,27 +10,19 @@ const shopRoutes = require("./routes/shop");
 
 const bodyParser = require('body-parser');
 
+const errorController = require("./controllers/404");
+
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.set('view engine', 'pug');*/
-
-/*
-const expressHBS = require("express-handlebars");
-app.engine("hbs", expressHBS());
-app.engine('hbs',expressHBS({layoutsDir:'views/layouts/', defaultLayout:'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');*/
-
 app.set('view engine', 'ejs');
+
 app.set('views', 'views');
 
 app.use('/admin', adminData.adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    /*res.status(404).sendFile(path.join(__dirname, "views", "404.html"));*/
-    res.status(404).render('404', {pageTitle:"404 Error", path:"404"});
-});
+app.use(errorController.get404);
 
 app.listen(2020);
