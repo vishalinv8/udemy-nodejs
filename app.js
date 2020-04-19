6260ba6d643943d46872c95199a6134aa41b14cd
Vishal <vishal.khanjan@gmail.com>
2020-04-19 21:16:24 +0530

    Added Order and Cart operation

--- app.js
+++ app.js
@@ -7,25 +7,24 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const adminRoutes = require("./routes/admin");
-
 const shopRoutes = require("./routes/shop");
-
 const staticRoutes = require("./routes/static");
+
+const errorController = require("./controllers/404");
 
 const sequelize = require('./utils/database');
 
 const Product = require('./models/product');
-
 const User = require('./models/user');
-
-const errorController = require("./controllers/404");
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({extended:false}));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-
 app.set('views', 'views');
 
 app.use((req, res, next) => {
@@ -38,36 +37,36 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin', adminRoutes);
-
 app.use(shopRoutes);
-
 app.use(staticRoutes);
-
 app.use(errorController.get404);
 
-
-
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-
 User.hasMany(Product);
-
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 //sequelize.sync({force: true})
 sequelize.sync()
-.then((results)=>{
-    return User.findByPk(1);
-})
-.then((user)=>{
-    if(!user){
-        return User.create({
-            name:"Vishal",
-            email:"vishal@mail.com"
-        });
-    }
-    return user;
-})
-.then((user)=>{
-    app.listen(2020);    
-}).catch((error)=>{
-    console.log(error);
-});
+    .then((results)=>{
+        return User.findByPk(1);
+    })
+    .then((user)=>{
+        if(!user){
+            return User.create({name:"Vishal",email:"vishal@mail.com"});
+        }
+        return user;
+    })
+    .then((user)=>{
+        return user.createCart();
+    })
+    .then((cart)=>{
+        app.listen(2020);    
+    }).catch((error)=>{
+        console.log(error);
+    });
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,5 @@
 const path = require("path");
 
-const fs = require("fs");
-
 const express = require("express");
 
 const adminController = require("../controllers/admin");
