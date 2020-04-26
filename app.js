4d7d81b23b0102c3ec848f73eb079cffbbe9d71f
Vishal <vishal.khanjan@gmail.com>
2020-04-26 22:13:37 +0530

    Mongodb Package installed and Connected remotely

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 const staticRoutes = require("./routes/static");
 
 const errorController = require("./controllers/404");
 
-const sequelize = require('./utils/database');
+const mongoConnect = require('./utils/database');
 
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+// const Order = require('./models/order');
+// const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -28,45 +28,20 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-    .then(user => {
-        req.user = user;
-        next();
-      })
-    .catch(err => console.log(err));
+    // User.findByPk(1)
+    // .then(user => {
+    //     req.user = user;
+    //     next();
+    //   })
+    // .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+
 app.use(staticRoutes);
 app.use(errorController.get404);
-
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-//sequelize.sync({force: true})
-sequelize.sync()
-    .then((results)=>{
-        return User.findByPk(1);
-    })
-    .then((user)=>{
-        if(!user){
-            return User.create({name:"Vishal",email:"vishal@mail.com"});
-        }
-        return user;
-    })
-    .then((user)=>{
-        return user.createCart();
-    })
-    .then((cart)=>{
-        app.listen(2020);    
-    }).catch((error)=>{
-        console.log(error);
-    });
+mongoConnect((client)=>{
+    console.log(client);
+    app.listen(2020);
+});
--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('udemy-nodejs', 'root', '', {
-    dialect: 'mysql', 
-    host:'localhost'
-});
-
-module.exports = sequelize;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const mongoConnect = (callback) => {
+    MongoClient.connect("mongodb+srv://mongodb_nodejs:OBkbw5XStRbCQef4@cluster0-shqot.mongodb.net/test?retryWrites=true&w=majority")
+    .then((client)=>{
+        console.log("MongoDB connected!");
+        callback(client);
+    })
+    .catch((err)=>{
+        console.log("Mongodb connection err", err);
+    });    
+};
+module.exports = mongoConnect;
